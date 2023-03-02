a74d5a86f7b2f952fe4a06d75c10ea10ecbb6731
mfadhili <mfadhili@github.com>
2023-03-02 23:05:09 +0300

    style: align header Logo to center

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { MidNavComponent } from './components/shared/mid-nav/mid-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MidNavComponent
   ],
   imports: [
     BrowserModule,
