cbfee7c1d769fbba8908aefb8f7c4597e7d971d2
mfadhili <mfadhili@github.com>
2023-03-03 21:15:42 +0300

    feat: added routes for various pages of mid nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CareGiversComponent} from "./components/care-givers/care-givers.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {TherapyRecordsComponent} from "./components/therapy-records/therapy-records.component";
+import {ChildrenComponent} from "./components/children/children.component";
+import {HelpingHandsComponent} from "./components/helping-hands/helping-hands.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: DashboardComponent},
+  {path: 'caregiver', component: CareGiversComponent},
+  {path: 'therapy_records', component: TherapyRecordsComponent},
+  {path: 'children', component: ChildrenComponent},
+  {path: 'helping_hands', component: HelpingHandsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
